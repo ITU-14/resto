04fc7ee3ac5867b4b414f7ecd4aefb9884c362f1
Christophe <lai_christophe@hotmail.com>
2019-04-03 10:17:14 +0300

    {RESTO-REACT}[F.O] List Resto in front

--- src/components/Front/Restos.js
+++ src/components/Front/Restos.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { compose } from 'recompose';
 import { withStyles, Paper, Card, Typography, TextField, Button, CardMedia, CardContent, CardActions } from '@material-ui/core';
 import { Search, Map } from '@material-ui/icons';
+import { IconButton, Table, TableHead, TableCell, TableRow, TableBody, TableFooter, TablePagination } from '@material-ui/core';
+import { Edit, Delete } from '@material-ui/icons';
+
+import { withFirebase } from '../Firebase';
 
 const styles = theme => ({
     paper: {
@@ -13,7 +17,7 @@ const styles = theme => ({
         paddingTop: theme.spacing.unit * 3,
         paddingBottom: theme.spacing.unit * 3
     },
-    
+
     container: {
         display: 'flex',
         flexWrap: 'wrap',
@@ -21,8 +25,8 @@ const styles = theme => ({
         marginLeft: 'auto',
         marginRight: 'auto',
         marginBottom: theme.spacing.unit * 3,
-        paddingLeft: theme.spacing.unit *8,
-        paddingRight: theme.spacing.unit *3
+        paddingLeft: theme.spacing.unit * 8,
+        paddingRight: theme.spacing.unit * 3
     },
     textField1: {
         marginRight: theme.spacing.unit,
@@ -42,7 +46,7 @@ const styles = theme => ({
         height: 140,
     },
     searchText: {
-        marginLeft: '7px', 
+        marginLeft: '7px',
         color: '#FFF'
     },
     cover: {
@@ -73,13 +77,66 @@ class Restos extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            test: true
+            loading: false,
+            open: true,
+            selectedIndexInList: 0,
+            openDeleteDialog: false,
+            openEditDialog: false,
+            page: 0,
+            restos: [],
+            rowsPerPage: 5
         }
+    }
+    componentDidMount() {
+        this.setState({ loading: true });
+        this.props.firebase.restos().on('value', snapshot => {
+            const restosObject = snapshot.val();
+            const restosList = Object.keys(restosObject).map(key => ({
+                ...restosObject[key],
+                id: key
+            }));
+            this.setState({
+                restos: restosList,
+                loading: false
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.props.firebase.restos().off();
+    }
+
+    handleOpenDeleteDialog = () => {
+        this.setState({ openDeleteDialog: true });
+    }
+
+    handleOpenEditDialog = () => {
+        this.setState({ openEditDialog: true });
+    }
+
+    handleCloseEditDialog = () => {
+        this.setState({ openEditDialog: false });
+    }
+
+    handleCloseDeleteDialog = () => {
+        this.setState({ openDeleteDialog: false });
+    }
+
+    handleChangePage = (event, page) => {
+        this.setState({ page });
+    }
+
+    handleChangeRowsPerPage = (event) => {
+        this.setState({ page: 0, rowsPerPage: event.target.value });
+    }
+
+    onDropImage = () => {
+
     }
 
     render() {
-        const {classes} = this.props;
-
+        const { classes } = this.props;
+        const { restos, page, rowsPerPage } = this.state;
         return (
             <Paper className={classes.paper}>
                 <Typography component="h2" variant="h5" className={classes.searchTitle}>
@@ -87,26 +144,26 @@ class Restos extends Component {
                 </Typography>
 
                 <br />
-                <form className={classes.container}>                      
-                    <TextField 
-                        id="nomResto" 
-                        label="Nom" 
-                        type="text" 
-                        name="nomResto" 
-                        variant="outlined" 
+                <form className={classes.container}>
+                    <TextField
+                        id="nomResto"
+                        label="Nom"
+                        type="text"
+                        name="nomResto"
+                        variant="outlined"
                         className={classes.textField1}
                     />
-                
-                    <TextField 
-                        id="typeCuisine" 
-                        label="Type de cuisine" 
-                        type="text" 
-                        name="typeCuisine" 
+
+                    <TextField
+                        id="typeCuisine"
+                        label="Type de cuisine"
+                        type="text"
+                        name="typeCuisine"
                         variant="outlined"
-                        className={classes.textField} 
+                        className={classes.textField}
                     />
-                    
-                    <Button 
+
+                    <Button
                         variant="contained"
                         type="submit"
                         color="primary"
@@ -116,40 +173,65 @@ class Restos extends Component {
                         <Typography className={classes.searchText}>
                             Rechercher
                         </Typography>
-                        
+
                     </Button>
                 </form>
-                <Card className={classes.card}>
-                    <CardMedia
-                        className={classes.cover}
-                        image="/assets/img/joystick_318-1404.jpg"
-                        title="Live from space album cover"
-                    />
+                {restos.slice(page * rowsPerPage, (page * rowsPerPage) + rowsPerPage).map(resto => (
                     
-                    <CardContent className={classes.content}>
-                        <Typography component="h5" variant="h5">
-                            Jean's Resto
+                <Card className={classes.card} >
+                  
+
+                        <CardMedia
+                            className={classes.cover}
+                            image="/assets/img/joystick_318-1404.jpg"
+                            title="Live from space album cover"
+                        />
+
+                        
+                 
+                        <CardContent className={classes.content}>
+                            <Typography component="h5" variant="h5">
+                            {resto.nom_resto}
                         </Typography>
-                        <Typography variant="subtitle1" color="textSecondary">
-                            Cuisine chinoise
+                            <Typography variant="subtitle1" color="textSecondary">
+                            {resto.type_cuisine}
                         </Typography>
-                        <Typography variant="subtitle2" color="textSecondary">
-                            St.Jean Road, Quatres Bornes
+                            <Typography variant="subtitle2" color="textSecondary">
+                            {resto.adresse}
                         </Typography>
-                        <Typography variant="subtitle2" color="textSecondary">
-                        +230 5874 9695
+                            <Typography variant="subtitle2" color="textSecondary">
+                            {resto.telephone}
                         </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardButton}>
-                        <Button color="primary">
-                            <Map/>
-                            Voir la carte
+                        </CardContent>
+                        <CardActions className={classes.cardButton}>
+                            <Button color="primary">
+                                <Map />
+                                Voir la carte
                         </Button>
-                    </CardActions>
+                        </CardActions>
+                     
                 </Card>
+                
+   ))}
+                <Table className={classes.table}>
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                rowsPerPageOptions={[5, 10, 25]}
+                                colSpan={3}
+                                count={restos.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                SelectProps={{ native: true }}
+                                onChangePage={this.handleChangePage}
+                                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                labelDisplayedRows={({ from, to, count }) => `${from} - ${to} sur ${count} restos`}
+                                labelRowsPerPage="Lignes par page" />
+                        </TableRow>
+                    </TableFooter>
+                </Table>
             </Paper>
         );
     }
 }
-
-export default compose(withStyles(styles))(Restos);
+export default compose(withFirebase, withStyles(styles))(Restos);
