88a3df915ecd32ada27a0b9aad2f03d2bd7465c6
Christophe <lai_christophe@hotmail.com>
2019-04-20 14:24:09 +0300

    Format Code : Singout

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -4,7 +4,7 @@ import { withFirebase } from '../Firebase';
 import { IconButton } from '@material-ui/core';
 import { PowerSettingsNew } from '@material-ui/icons';
 
-const SignOutButton = ({firebase}) => (
+const SignOutButton = ({ firebase }) => (
   <IconButton color="inherit" onClick={firebase.logout} title="Se d&eacute;connecter">
     <PowerSettingsNew />
   </IconButton>
